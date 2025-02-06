35f9d9d8a79f73502f3fc5c6132376eda35d7ad6
oreinke <159829836+oreinke@users.noreply.github.com>
2025-02-06 15:34:38 -0500

    Time: 862 ms (8.98%) | Memory: 100.6 MB (5.57%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -0,0 +1,30 @@
+typedef unsigned long long ull;
+
+class Solution {
+public:
+    
+    ull factorial (int n, map<ull, ull> &fact) {
+        if (fact.find(n) != fact.end()) return fact[n];
+        else return factorial(n - 1, fact) * n;
+    }
+
+    int tupleSameProduct(vector<int>& nums) {
+        map<ull, ull> fact;
+        fact[1] = 1;
+        fact[0] = 1;
+        map<int, int> products;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                products[nums[i] * nums[j]]++;
+            }
+        }
+
+        long long cnt = 0;
+        for (auto [x, y] : products) {
+            if (y > 1) {
+                cnt += (factorial(y, fact) / (2 * factorial(y - 2, fact))) * 8;
+            }
+        }
+        return cnt;
+    }
+};
