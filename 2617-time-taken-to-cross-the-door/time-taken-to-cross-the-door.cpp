fc3ec9cb27fb18e644203cd05ab71554e17b5bcd
oreinke <159829836+oreinke@users.noreply.github.com>
2025-02-25 10:09:40 -0500

    Time: 338 ms (6.55%) | Memory: 82.5 MB (54.18%) - LeetSync

---
+++ 2617-time-taken-to-cross-the-door/time-taken-to-cross-the-door.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    vector<int> timeTaken(vector<int>& arrival, vector<int>& state) {
+        int n = arrival.size();
+        queue<int> enter, exit;
+        vector<int> answer (n);
+        int time = arrival[0];
+        bool e = true;
+        int i = 0;
+        while (i < n) {
+            if (arrival[i] > time) {
+                 e = true;
+                time = arrival[i];
+            }
+
+            while (i < n && arrival[i] <= time) {
+                if (1 == state[i]) {
+                    exit.push(i);
+                }
+                else {
+                    enter.push(i);
+                }
+
+                ++i;
+            }
+
+            while (!exit.empty() || !enter.empty()) {
+                printf("i: %d, time: %d\n", i, time);
+                fflush(stdout);
+                while (i < n && arrival[i] <= time) {
+                    
+                    if (1 == state[i]) {
+                        exit.push(i);
+                    }
+                    else {
+                        enter.push(i);
+                    }
+
+                    ++i;
+                }
+
+                if (exit.empty() || enter.empty()) {
+                    
+                    if (exit.empty()) {
+                        int ind = enter.front(); enter.pop();
+                        answer[ind] = time;
+                        ++time;
+                        e = false;
+                    }
+                    else {
+                        int ind = exit.front(); exit.pop();
+                        answer[ind] = time;
+                        ++time;
+                        e = true;
+                    }
+                }
+                else if (e) {
+                    int ind = exit.front(); exit.pop();
+                    answer[ind] = time;
+                    ++time;
+    
+                }
+                else {
+                    int ind = enter.front(); enter.pop();
+                    answer[ind] = time;
+                    ++time;
+                }
+            }
+        }
+        return answer;
+    }
+};
