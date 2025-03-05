44a33f73b4db6654619a482a6693ed86cae50fad
oreinke <159829836+oreinke@users.noreply.github.com>
2025-03-05 17:20:41 -0500

    Time: 8 ms (65.40%) | Memory: 38.3 MB (83.11%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,33 +2,13 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         map<int, int> cnt;
-        for (char c : tasks) cnt[c - 'a']++;
-        deque<tuple<int, int, int>> backlog;
-        priority_queue<pair<int, int>> pq;
-        for (auto[x, y] : cnt) {
-            pq.push({y, x});
-        }
-        int time = 0;
-        while (!pq.empty() || !backlog.empty()) {
-            if (!pq.empty()) {
-                auto [x, y] = pq.top(); pq.pop();
-                if (x != 1) {
-                    backlog.push_back({n + 1, x - 1, y});
-                }
-            }
-
-            deque<tuple<int, int, int>> nextLog;
-            for (auto [delay, count, task] : backlog) {
-                if (delay == 1) {
-                    pq.push({count, task});
-                }
-                else {
-                    nextLog.push_back({delay - 1, count, task});
-                }
-            }
-            backlog = nextLog;
-            time++;
-        }
+        for (char c : tasks)  cnt[c - 'a']++;
+        int maxCnt = 0;
+        for (auto [x, y] : cnt) maxCnt = max(maxCnt, y);
+        int time = (maxCnt - 1) * (n + 1);
+        for (auto [x, y] : cnt) if (y == maxCnt) time++;
+        if (time < tasks.size()) return tasks.size();
+        
         return time;
     }
 };
