82b2eaf5acfb2ed57e6b591b195009e89a4aeb52
oreinke <159829836+oreinke@users.noreply.github.com>
2025-03-06 14:44:06 -0500

    Time: 0 ms (100.00%) | Memory: 10 MB (26.13%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.c
+++ 88-merge-sorted-array/merge-sorted-array.c
@@ -1,47 +1,30 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int *arr;
-    int i, j;
-
-    if ( m + n == 0)
-    {
-        return;
-    }
-
-    arr = malloc(sizeof(int) * m);
-    for (i = 0; i < m; i++)
-    {
-        arr[i] = nums1[i];
-    }
-
-    i = 0;
-    j = 0;
-    while ( i < m && j < n)
-    {
-        if (arr[i] < nums2[j])
-        {
-            nums1[i + j] = arr[i];
-            i++;
+    n--;
+    m--;
+    int curi = nums1Size - 1;
+    while (n >= 0 && m >= 0) {
+        if (nums1[m] > nums2[n]) {
+            nums1[curi] = nums1[m];
+            m--;
         }
-        else
-        {
-            nums1[i + j] = nums2[j];
-            j++;
+        else {
+            nums1[curi] = nums2[n];
+            n--;
         }
+        curi--;
     }
 
-    while (i < m)
-    {
-        nums1[i + j] = arr[i];
-        i++;
+    while (m >= 0) {
+        nums1[curi] = nums1[m];
+        m--;
+        curi--;
     }
 
-    while (j < n)
-    {
-        nums1[i + j] = nums2[j];
-        j++;
+    while (n >= 0) {
+        nums1[curi] = nums2[n];
+        n--;
+        curi--;
     }
-
-    free(arr);
     return;
 
 }
