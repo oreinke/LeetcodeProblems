daa8bb8e5f16a0b869c51d06bb71d5bc8ea68a42
oreinke <159829836+oreinke@users.noreply.github.com>
2026-04-22 20:06:20 -0400

    Time: 22 ms (66.88%) | Memory: 21.9 MB (45.62%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.rbegin(), coins.rend());
         vector<long long> dp (amount + 1, 1e18);
         dp[0] = 0;
         for (int i = 1; i <= amount; ++i) {
